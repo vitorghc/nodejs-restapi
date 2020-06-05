bef958b8132a3e04e7cc5faf166bbd8147210437
Vitor Feltrin <vitor.feltrin@sensedia.com>
2020-06-05 17:00:19 -0300

    crud products

---
+++ server.js
@@ -0,0 +1,20 @@
+const express   = require("express");
+const mongoose  = require("mongoose");
+const requireDir = require("require-dir");
+const cors = require("cors");
+
+//iniciando app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando db
+mongoose.connect("mongodb://localhost:27017/nodeapi",
+ {useNewUrlParser: true, useUnifiedTopology: true});
+
+requireDir("./src/models/")
+
+//Rotas
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
